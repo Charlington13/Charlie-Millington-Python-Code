dd331d4d19d877c12d594f4603157fd39d80681b
Charlie Millington <67446068+Charlington13@users.noreply.github.com>
2020-06-28 02:47:31 +0100

    Update numbercounter.py

--- numbercounter.py
+++ numbercounter.py
@@ -2,13 +2,17 @@ largest = None
 smallest = None
 while True:
     num = input("Enter a number: ")
-    if num == "done": break
+    if num == "done": 
+        break
     try:
         num = int(num)
-        if largest is None or largest < num: largest = num
-        if smallest is None or smallest > num: smallest = num
+        if largest is None or largest < num: 
+            largest = num
+        if smallest is None or smallest > num:
+            smallest = num
     except:
         print ("Invalid input")
         continue
+        
 print ("Maximum is",largest)
 print ("Minimum is",smallest)
